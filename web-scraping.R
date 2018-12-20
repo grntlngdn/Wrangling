7ba4c2e4bd95384bc80f29917f0a5ec879b36479
grntlngdn <grntlngdn@gmail.com>
2018-12-19 22:51:22 -0600

    The lecture example of using rvest

--- web-scraping.R
+++ web-scraping.R
@@ -1,1 +1,8 @@
 library(rvest)
+url <- paste0("https://en.wikipedia.org/w/index.php?title=",
+              "Gun_violence_in_the_United_States_by_state&oldid=843135608")
+h <- read_html(url)
+tab <- h %>% html_nodes("table")
+tab <- tab[[3]] %>% html_table
+tab <- tab %>% setNames(c("state", "population", "total", "murder_rate"))
+head(tab)
