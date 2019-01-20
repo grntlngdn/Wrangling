a3d266edc19e890e4febbde6bae5f7df7b315642
grntlngdn <grntlngdn@gmail.com>
2019-01-19 21:41:15 -0600

    Added data points to plot. This revealed that some pages tibbles had rows that came from the x-axis of the plot, so this was fixed. All the string processing was piped together and comments were moved to the right of the page to fit with this better. Changed how each lines string was split from using tabs to fix what year february 29ths murders were associated with. Used lapply instead of for loop.

--- wrangling-exc/string-processing/analysis.R
+++ wrangling-exc/string-processing/analysis.R
@@ -2,5 +2,6 @@ library(tidyverse)
 
 load("rdas/mortality.rda")
 mortality %>% ggplot(aes(date, deaths)) +
+  geom_point(alpha = 0.2) +
   geom_smooth()
 ggsave("figs/plot.png")
--- wrangling-exc/string-processing/wrangle-data.R
+++ wrangling-exc/string-processing/wrangle-data.R
@@ -4,51 +4,27 @@ library(pdftools)
 filename = "data/mortality.pdf"
 dest_file = "rdas/mortality.rda"
 
-mortality_pdf = pdf_text(filename)
-
-mortality = data.frame()
-
-for(page in 1:length(mortality_pdf)) {
-  mortality_raw =  mortality_pdf %>%
-    .[[page]] %>%
-    str_split("\n") %>%
-    .[[1]] %>%
-    str_trim()
-  
-  #removes header and tail
-  header_index = str_which(mortality_raw, "^[A-Z]{3}\\s+2015\\s+2016\\s+2017\\s+2018")
-  header =  mortality_raw[header_index] %>%
-    str_split("\\s+", simplify = T) %>%
-    .[1,]
-  
-  
-  tail_index = str_which(mortality_raw, "Total")
-  
-  mortality_raw = mortality_raw[(header_index+1):(tail_index-1)]
-  
-  #removes rows with just one number (from the y-axis of the plot)
-  n = 1<str_count(mortality_raw, "\\d+")
-  
-  #removes part of strings from right of each page and converts
-  #text from the table on the left of each page into one tidy tibble
-  month = header[1]
-  header[1] = 'day'
-  mortality_raw = mortality_raw[n] %>%
-    str_remove_all("[^\\d\\s]") %>%
-    str_split_fixed("\\s+", 6) %>%
-    .[,1:5] %>%
-    apply(c(1,2),as.numeric) %>%
-    as_tibble() %>%
-    setNames(header) %>%
-    mutate(month_abb = month, month = page) %>%
-    gather(year, deaths, header[2:5]) %>%
-    mutate(date = ISOdate(year, month, day)) %>%
-    filter(!is.na(deaths), date < ISOdate(2018,5,31))
-  mortality = bind_rows(mortality, mortality_raw)
-}
+mortality = pdf_text(filename) %>%
+  str_split("\n")# %>% #                              #turns vector of pages into list of pages represented as vectors of lines
+  mortality = lapply (seq_along(mortality), function(page) {
+    mortality[[page]] %>% str_trim %>% #              #applies the following to each page of the pdf
+      .[(str_which(., "^[A-Z]{3}\\s+2015\\s+2016\\s+2017\\s+2018") + 1):#starts subset selection after header index
+          (str_which(., "Total") - 1)] %>% #          #ends subset slection before summary section
+      .[1<str_count(., "\\d+")] %>% #                 #deletes rows with just one number (from the y-axis of the plot)
+      str_remove_all("[^\\d\\s]") %>% #               #removes text (from right of each page)
+      str_split_fixed(" {3,6}", 6) %>% .[,1:5] %>% #  #leaves nearly just table (from left of each page) as matrix
+      apply(c(1,2),as.numeric) %>% #                  #converts entries of that matrix to numeric
+      as_tibble() %>% #                               #each matrix to tibble
+      filter(!(V1 == 1 & V2 == 2 & V3 == 3 & V4 == 4 & V5 == 5)) %>% #removes row that came from x-axis of the plot
+      setNames(c('day', 2015, 2016, 2017, 2018)) %>%  #column names
+      mutate(month = page) %>% #                      #creates month variable
+      gather(year, deaths, header[2:5]) %>% #         #tidies data to make each date a seperate observation
+      mutate(date = ISOdate(year, month, day)) %>% #  #creates date variable
+      filter(date < ISOdate(2018,5,31)) #             #remove zeroes used as fillers after data stops may 31, 2018
+  }
+  ) %>% bind_rows() #                                 #bind all months' observations into a single dataframe
 
 
-mortality %>%
-  filter(deaths < 8)
+mortality
 
 save(mortality, file = dest_file)
